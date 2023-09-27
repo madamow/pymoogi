385b896b0e20c9a5bc3ad6b397f9e24feb13b23c
Monika Adamow <madamow@illinois.edu>
2023-09-27 13:05:13 -0500

    More fstrings

--- pymoogi/lib/Common_functions.py
+++ pymoogi/lib/Common_functions.py
@@ -33,17 +33,17 @@ def get_key(item):
 
 def istype(value, expect_type='float'):
     # Check if value is a float
-    if  expect_type == 'float':
+    if expect_type == 'float':
         try:
             float(value)
             return True
         except ValueError:
             return False
     elif expect_type == 'int':
-         try:
+        try:
             int(value)
             return True
-         except ValueError:
+        except ValueError:
             return False
     else:
         return False
@@ -53,7 +53,7 @@ def check_if_number(checkval=None, expect_type='float'):
     if not checkval:
         checkval = input()
     while not istype(checkval, expect_type=expect_type):
-        print(f"Your value is not a(n) {expect_type}. Try again")
+        print(f"Your value is not a(n) {expect_type} type number. Try again")
         checkval = input()
 
     if expect_type == 'float':
@@ -100,16 +100,15 @@ def dict_to_str(dict_par):
         no_el = str(len(dict_par['abundances']))
         s = s + "abundances " + no_el + " " + str(dict_par['syn_no']) + "\n"
         for elem in dict_par['abundances']:
-            s = s + "    " + str(elem) + "    "
-            s = s + (' '.join(list(map(str, dict_par['abundances'][elem])))) + "\n"
+            ab_string = ' '.join(list(map(str, dict_par['abundances'][elem])))
+            s = s + "    " + str(elem) + "    " + ab_string + "\n"
 
     if 'isotopes' in list(dict_par.keys()):
         no_el = str(len(dict_par['isotopes']))
-
         s = s + "isotopes " + no_el + " " + str(dict_par['syn_no']) + "\n"
         for elem in dict_par['isotopes']:
-            s = s + "    " + str(elem) + "    "
-            s = s + (' '.join(list(map(str, dict_par['isotopes'][elem])))) + "\n"
+            iso_string = ' '.join(list(map(str, dict_par['isotopes'][elem])))
+            s = s + "    " + str(elem) + "    " + iso_string + "\n"
 
     if 'synlimits' in list(dict_par.keys()):
         s = s + "synlimits\n"
@@ -136,9 +135,9 @@ def run_moog(pars):
 
 
 def print_driver(driver):
-    print("****************************************************************************")
+    print("**************************************************************")
     print("MOOG IS CONTROLLED BY DRIVER", driver)
-    print("****************************************************************************\n")
+    print("**************************************************************\n")
 
 
 def id_generator(size=3, chars=string.ascii_uppercase + string.digits):
@@ -153,23 +152,32 @@ def do_stats(x, y):
 
 def print_stats(t):
     print("Here are abundances for", ELEMENTS[int(t[0, 1])])
-    print("%10s %5s %8s %8s %8s %7s %7s %8s" %
-          ("wavelength", "ID", "EP", "logGF", "EWin", "logRWin", "abund", "delavg"))
-    for l in t[t[:, 0].argsort()]:
-        print("%10.2f %5.1f %8.2f %8.2f %7.2f %7.3f %8.3f %8.3f" %
-                  (l[0], l[1], l[2], l[3], l[4], l[5], l[6], l[7]))
+    print(f"{'wavelength':>10s} {'ID':>5s} {'EP':>8s} {'logGF':>8s} "
+          f"{'EWin':>8s} {'logRWin':>7s} {'abund':>7s} {'delavg':>8s}")
+
+    for item in t[t[:, 0].argsort()]:
+        print(f"{item[0]:>10.2f} {item[1]:>5.1f} {item[2]:>8.2f}"
+              f"{item[3]:>8.2f} {item[4]:>7.2f} {item[5]:>7.3f} "
+              f"{item[6]:>8.3f} {item[7]:>8.3f}")
     if t.shape[0] == 1.:
         print("Just one line, no stats")
     else:
-        print("average abundance  = %-9.3f std. deviation =  %-9.3f #lines = %3i" %
-              (np.average(t[:, 6]), np.std(t[:, 6], ddof=1), len(t[:, 6])))
+        print(f"average abundance  = {np.average(t[:, 6]):>-9.3f} "
+              f"std. deviation =  {np.std(t[:, 6], ddof=1):>-9.3f} "
+              f"#lines = {len(t[:, 6]):3d}")
     if t.shape[0] > 3.:
-        print("E.P. correlation:  slope = %7.3f  intercept = %7.3f  corr. coeff. = %7.3f" %
-              (do_stats(t[:, 2], t[:, 6])))
-        print("R.W. correlation:  slope = %7.3f  intercept = %7.3f  corr. coeff. = %7.3f" %
-              (do_stats(t[:, 5], t[:, 6])))
-        print("wav. correl.:  slope = %7.3e  intercept = %7.3f  corr. coeff. = %7.3f\n" %
-              (do_stats(t[:, 0], t[:, 6])))
+        ep_stats = do_stats(t[:, 2], t[:, 6])
+        print(f"E.P. correlation:  slope = {ep_stats[0]:>7.3f}  "
+              f"intercept = {ep_stats[1]:>7.3f}  "
+              f"corr. coeff. = {ep_stats[2]:>7.3f}")
+        rw_stats = do_stats(t[:, 5], t[:, 6])
+        print(f"R.W. correlation:  slope = {rw_stats[0]:>7.3f}  "
+              f"intercept = {rw_stats[1]:>7.3f}  "
+              f"corr. coeff. = {rw_stats[2]:>7.3f}")
+        wav_stats = do_stats(t[:, 0], t[:, 6])
+        print(f"wav. correl.:  slope = {wav_stats[0]:>7.3f}  "
+              f"intercept = {wav_stats[1]:>7.3f}  "
+              f"corr. coeff. = {wav_stats[2]:>7.3f}")
     else:
         print("No statistics done for E.P. trends")
         print("No statistics done for R.W. trends")
